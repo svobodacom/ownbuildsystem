ebf5c4f909908fb537982490a373c6a2fad07cec
Don Silvio <svobodacom1@gmail.com>
2025-08-25 17:21:16 +0300

    added #define attribute for linux

--- Character.h
+++ Character.h
@@ -1,11 +1,15 @@
 #pragma once
 
-#ifdef _EXPORTING
-#define GAME_API __declspec(dllexport)
-#elif _IMPORTING
-#define GAME_API __declspec(dllimport)
+#if defined(WIN32) || defined(_WINDOWS)
+    #ifdef _EXPORTING
+        #define GAME_API __declspec(dllexport)
+    #elif _IMPORTING
+        #define GAME_API __declspec(dllimport)
+    #else
+        #define GAME_API
+    #endif
 #else
-#define GAME_API
+    #define GAME_API __attribute__((visibility ("default")))
 #endif
 
 class Weapon;
--- Math.h
+++ Math.h
@@ -1,11 +1,15 @@
 #pragma once
 
-#ifdef _EXPORTING
-#define MATH_API __declspec(dllexport)
-#elif _IMPORTING
-#define MATH_API __declspec(dllimport)
+#if defined(WIN32) || defined(_WINDOWS)
+    #ifdef _EXPORTING
+        #define MATH_API __declspec(dllexport)
+    #elif _IMPORTING
+        #define MATH_API __declspec(dllimport)
+    #else
+        #define MATH_API
+    #endif
 #else
-#define MATH_API
+    #define MATH_API __attribute__((visibility ("default")))
 #endif
 
 namespace LifeExe
